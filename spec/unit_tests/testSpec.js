6a8d927aab71fc4a350b5c5d0502ef0190984737
Robert Long <rlong@sfox.com>
2022-12-21 10:58:19 -0500

    Finish transfer functionality

--- spec/unit_tests/testSpec.js
+++ spec/unit_tests/testSpec.js
@@ -1,14 +1,17 @@
 import Bank from "../../lib/Bank.js";
 import SavingsAccount from "../../lib/accounts/savingsAccount.js";
+import IndividualAccount from "../../lib/accounts/individualAccount.js";
 
 describe('Bank tests', function() {
   let bank;
   let savingsAccount;
+  let individualAccount;
   beforeEach(function() {
     bank = new Bank('The Vault');
     savingsAccount = new SavingsAccount('Mr. Moneybags');
+    individualAccount = new IndividualAccount('Mrs. Moneybags');
     bank.addAccount(savingsAccount);
-    bank.addAccount(savingsAccount);
+    bank.addAccount(individualAccount);
   });
 
   it('should allow you to deposit to the savings account', function() {
@@ -25,6 +28,16 @@ describe('Bank tests', function() {
     savingsAccount.deposit(depositAmount);
     savingsAccount.withdraw(withdrawalAmount)
     expect(savingsAccount.balance).toEqual(expectedBalance);
+  });
+
+  it('should allow you to transfer funds between accounts', function() {
+    const depositAmount = 50000;
+    const transferAmount = 2000;
+    const expectedBalance = depositAmount - transferAmount;
+    savingsAccount.deposit(depositAmount);
+    savingsAccount.transfer(transferAmount, individualAccount);
+    expect(savingsAccount.balance).toEqual(expectedBalance);
+    expect(individualAccount.balance).toEqual(transferAmount);
   });
 });
   
